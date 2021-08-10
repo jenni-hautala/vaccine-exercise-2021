f4e9624aee6f609e1a875801c10c99b18ca10a2d
Jenni Hautala <jenni.hautala@viidakko.fi>
2021-08-10 14:36:09 +0300

    Connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,3 +1,26 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(express.json({
+	limit: "30mb",
+	extended: true
+}));
+app.use(express.urlencoded({
+	limit: "30mb",
+	extended: true
+}));
+app.use(cors());
+
+// Connect to MongoDB Atlas
+
+const CONNECTION_URL = 'mongodb+srv://patalapanen:lapatapanen@cluster0.yw7iw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() =>  app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+	.catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
