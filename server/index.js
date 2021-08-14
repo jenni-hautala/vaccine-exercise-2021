faf070d8f70cb47092433c7a8bea1effe624bb71
Jenni Hautala <jenni.hautala@viidakko.fi>
2021-08-14 17:18:15 +0300

    added heroku

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import vaccRoutes from './routes/vaccinations.js';
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({
 	limit: "30mb",
@@ -20,10 +22,9 @@ app.use(cors());
 app.use('/vaccinations', vaccRoutes);
 
 // Connect to MongoDB Atlas
-const CONNECTION_URL = 'mongodb+srv://patalapanen:lapatapanen@cluster0.yw7iw.mongodb.net/Vaccinates?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() =>  app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 	.catch((error) => console.log(error.message));
 
